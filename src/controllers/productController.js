8e9e0c5579e3f2e79f309c1ba1e9524b0430c78a
Wahyu Ramadan <wramadan1203@gmail.com>
2023-02-28 15:16:36 +0700

    condition if fail update

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -73,7 +73,7 @@ const updateProductByIdController = async (req, res) => {
 
     const update = await updateProductByIdModel(id, data);
 
-    if (!update || !update[0].affectedRows) return res.sendWrapped('Fail to update product', {}, httpStatus.CONFLICT);
+    if (!update || !update.length || !update[0].affectedRows) return res.sendWrapped('Fail to update product', {}, httpStatus.CONFLICT);
 
     res.sendWrapped('Update product successfully', data, httpStatus.OK);
   } catch (error) {
