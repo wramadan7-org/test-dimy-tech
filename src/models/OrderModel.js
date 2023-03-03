f6972f4efe356dbdbb0019401db9b78641e7b56b
Wahyu Ramadan <wramadan1203@gmail.com>
2023-03-03 23:47:35 +0700

    comment

--- src/models/OrderModel.js
+++ src/models/OrderModel.js
@@ -3,7 +3,7 @@ const db = require('../configurations/db');
 const createOrderModel = async (data) => {
   try {
     // Need backtip (`) at column
-    // Use .query, because .execute is excape the values
+    // Use .query, because .execute is excape the values for multiple row
     const order = await (await db).query('INSERT INTO `order` (`order_code`, `customer_address_id`, `product_id`, `payment_method_id`) VALUES ?', [data]);
   
     return order;
