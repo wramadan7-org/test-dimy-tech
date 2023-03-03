f47d6b7cc13856e0e856670d3f8f0fb8084f23a3
Wahyu Ramadan <wramadan1203@gmail.com>
2023-03-03 23:45:49 +0700

    crete order

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,6 +1,9 @@
 const httpStatus = require('http-status');
+const { randomString } = require('../helpers/randomString');
 const { getCustomerAddressByIdModel } = require('../models/CustomerAddressModel');
 const { createOrderModel } = require('../models/OrderModel');
+const { getPaymentMethodByIdModel } = require('../models/PaymentMethodModel');
+const { getProductByIdModel } = require('../models/ProductModel');
 
 const createOrderController = async (req, res) => {
   try {
@@ -11,18 +14,44 @@ const createOrderController = async (req, res) => {
 
     const [rowsCustomerAddress, fieldsCustomerAddress] = isCustomerAddressValid;
 
-    if (!rowsCustomerAddress || !rowsCustomerAddress.length) return res.sendWrapped('Not found', {}, httpStatus.NOT_FOUND);
+    if (!rowsCustomerAddress || !rowsCustomerAddress.length) return res.sendWrapped('Customer address not found', {}, httpStatus.NOT_FOUND);
 
-    const mapBody = requestBody.map((o) => {
+    const random = await randomString(10);
+
+    // Use async in loop
+    const promises = requestBody.map(async (o) => {
+      const isProductValid = await getProductByIdModel(o.product_id);
+
+      const [rowsProduct, fieldsProduct] = isProductValid;
+
+      if (!isProductValid || !isProductValid.length) return res.sendWrapped('Product not found', {}, httpStatus.NOT_FOUND);
+
+      const isPaymentMethodValid = await getPaymentMethodByIdModel(o.payment_method_id);
+
+      const [rowsPaymentMethod, fieldsPaymentMethod] = isPaymentMethodValid;
+
+      if (!isPaymentMethodValid || !isPaymentMethodValid.length) return res.sendWrapped('Payment method not found', {}, httpStatus.NOT_FOUND);
+
       const data = {
+        order_code: random,
         customer_address_id: parseInt(customer_address_id),
         ...o,
       };
 
-      return data;
+      // Return only the value of data
+      return Object.values(data);
     });
 
-    res.sendWrapped('Order successfully', mapBody, httpStatus.CREATED);
+    // await the result of all loop and catch in promise.all
+    const data = await Promise.all(promises);
+
+    const order = await createOrderModel(data);
+
+    const [rowsOrder, fieldsOrder] = order;
+
+    if (!rowsOrder || !rowsOrder.affectedRows) return res.sendWrapped('Fail to create order', {}, httpStatus.CONFLICT);
+
+    res.sendWrapped('Order successfully', rowsOrder, httpStatus.CREATED);
   } catch (error) {
     throw error;
   }
--- src/models/OrderModel.js
+++ src/models/OrderModel.js
@@ -2,7 +2,9 @@ const db = require('../configurations/db');
 
 const createOrderModel = async (data) => {
   try {
-    const order = await (await db).execute('INSERT INTO order (order_code, customer_address_id, product_id, payment_method_id) VALUES(??)', [data]);
+    // Need backtip (`) at column
+    // Use .query, because .execute is excape the values
+    const order = await (await db).query('INSERT INTO `order` (`order_code`, `customer_address_id`, `product_id`, `payment_method_id`) VALUES ?', [data]);
   
     return order;
   } catch (error) {
